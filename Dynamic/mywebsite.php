0352827f4351264d2c267127526e0ed5f6383fd4
Gautham <95111246+Gautham248@users.noreply.github.com>
2023-11-24 09:08:27 +0530

    Add files via upload

---
+++ Dynamic/mywebsite.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website www.smith.com</title>
+<link href="mystyle.css" rel="stylesheet"/>
+</head>
+<body>
+    <?php
+    include 'myheader.html';
+    ?>
+    <div class="content">
+        <h2>Welcome to My Website www.smith.com</h2>
+        <p><img src="images/sjc.jpg" width="500"/></p>
+    </div>
+</body>
+</html>
+<?php
+include 'config.php';
+?>
